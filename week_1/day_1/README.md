812d5bf5df7bd43f0d7133cc8544f8095e7cac97
Daniel Meilak <daniel.mlak@gmail.com>
2021-12-01 14:34:23 +0100

    added alternate solution to day 1

--- week_1/day_1/README.md
+++ week_1/day_1/README.md
@@ -1,5 +1,7 @@
-## Day 1
+## Day 1 
 
-Start of AoC 2021!!
+Start of AoC 2021!! 
 
-Loop through input to check for increments. Used vector indices to find and sum depths. Later realised that part 2 can be siplified to compar depths which are 2 steps apart.
+Loop through input to check for increments. Used vector indices to find and sum depths. Later realised that part 2 can be siplified to compar depths which are 2 steps apart. 
+
+After looking at Reddit, found out about a very convenient function, `std::inner_product`, which can reduce the code length considerably. An alternate form of the solution is provided in the function `void using_inner_product()`. 
--- week_1/day_1/day_1.cpp
+++ week_1/day_1/day_1.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<algorithm>
 #include<cstdlib>
+#include<numeric>
 #include"../../Utils/utils.h"
 
 int main(){
@@ -34,4 +35,22 @@ int main(){
     std::cout << "Answer (part 2): " << part_2 << std::endl;
 
     return 0;
+}
+
+// Thanks to another reddit user, /u/paul2718, std::inner_product is a convenient inbuilt function that 
+// can shorted the code considerably. Of course you are now required to loop through the input twice.
+void using_inner_product(){
+
+    // read input into vector of strings.
+    std::vector<int> input = input_to_int<int>(read_input("input", ""));
+
+    // parameter breakdown:                                first1         last1          first2           init  op1            op2
+    std::cout << "Answer (part 1): " << std::inner_product(input.begin(), input.end()-1, input.begin()+1, 0   , std::plus<>(), std::less<>()) << "\n";
+    std::cout << "Answer (part 2): " << std::inner_product(input.begin(), input.end()-3, input.begin()+3, 0   , std::plus<>(), std::less<>()) << "\n";
+
+    // first1, last1 - first range of elemets
+    // first2        - beginning of second range
+    // init          - initial value of the sum
+    // op1           - takes return of op2 and accumulator(init) and produces a new value
+    // op2           - compares one value from each range and produces a new value
 }
