012cd55c16bcdaa6f22e4f552b6e34c128727e56
Daniel Meilak <daniel.mlak@gmail.com>
2021-12-12 20:47:34 +0100

    improved solution to day 12

--- week_2/day_12/day_12.cpp
+++ week_2/day_12/day_12.cpp
@@ -4,18 +4,17 @@
 #include<string>
 #include<cctype>
 #include<unordered_map>
-#include<unordered_set>
+#include<set>
 #include"../../Utils/utils.h"
 
 // struct to keep graph information together
 struct graph_t{
 
     // start and end of graph
-    std::string start = "start", end = "end";
+    const std::string start = "start", end = "end";
 
-    // special small cave and num visits to it
-    std::string special;
-    int special_visits = 0;
+    // flag to allow visiting small cave twice
+    bool part_2 = false;
 
     // map of nodes
     std::unordered_map<std::string,std::vector<std::string>> tree;
@@ -26,12 +25,9 @@ struct graph_t{
     // method to find number of paths
     void calculate_paths();
 
-    // method to find number of paths with small cave special case (part 2)
-    void calculate_paths_extra_cave();
-
     private:
         // breadth first search for looking through tree
-        void BFS( const std::string& current, std::vector<std::string>& path, std::unordered_set<std::string>& visited );
+        void BFS( const std::string& current, std::vector<std::string>& path, std::multiset<std::string>& visited, bool visited_twice = false );
 };
 
 int main(){
@@ -52,9 +48,11 @@ int main(){
     // number of normal paths (part 1)
     graph.calculate_paths();
     size_t part_1 = graph.paths.size();
+    graph.paths.clear();
 
     // number of paths with extra small cave (part 2)
-    graph.calculate_paths_extra_cave();
+    graph.part_2 = true;
+    graph.calculate_paths();
     size_t part_2 = graph.paths.size();
 
     std::cout << "Answer (part 1): " << part_1 << std::endl;
@@ -66,7 +64,7 @@ int main(){
 void graph_t::calculate_paths(){
 
     // visited nodes (uppercase nodes)
-    std::unordered_set<std::string> visited;
+    std::multiset<std::string> visited;
 
     // vector to store path
     std::vector<std::string> path;
@@ -75,47 +73,33 @@ void graph_t::calculate_paths(){
     BFS(start, path, visited);
 }
 
-void graph_t::calculate_paths_extra_cave(){
+void graph_t::BFS( const std::string& current, std::vector<std::string>& path, std::multiset<std::string>& visited, bool visited_twice ){
 
-    // loop through nodes to try different small caves
-    for ( const auto& [key, value] : tree ){
-
-        // skip start and end
-        if (key == "start" || key == "end" || std::isupper(key[0])){ continue; }
-
-        // set to special
-        special = key;
-
-        calculate_paths();
-    }
-}
-
-void graph_t::BFS( const std::string& current, std::vector<std::string>& path, std::unordered_set<std::string>& visited ){
-
-    // if small cave check if it can be visited twice, if not add to visited
+    // if small cave add to visited
     if ( std::islower(current[0]) ){
-        if (current != special){ visited.insert(current); }
-        else if ( ++special_visits == 2 ){ visited.insert(current); }          
+        // for part 2, one small cave can be visited twice
+        if (part_2 && visited.contains(current)){ visited_twice = true; }
+        visited.insert(current);
     }
 
     // add current step to path
     path.push_back(current);
 
     // if at end, add full path to paths
-    if ( current == end ){
-        if (std::find(paths.begin(),paths.end(),path) == paths.end()){ paths.push_back(path); }
-    }
+    if ( current == end ){ paths.push_back(path); }
     // else, continue BFS with connected nodes
     else {
         for (const std::string& next : tree[current]){
 
-            // only continue to unvisited or uppercase nodes
-            if (!visited.contains(next)){ BFS(next, path, visited); }
+            if (next == start){ continue; }
+
+            // only continue to unvisited or uppercase nodes, and in part 2, one extra small cave
+            if (!visited.contains(next) || (part_2 && !visited_twice)){ BFS(next, path, visited, visited_twice); }
         }
     }
 
     // remove last step and visited check for other paths
     path.pop_back();
-    if (current == special){ special_visits--; }
-    visited.erase(current);
+    auto it = visited.find(current);
+    if (it != visited.end()){ visited.erase(it); }
 }
