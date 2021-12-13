197ea057e79b58904f5b675a6b9a0fc5a51dc15d
Daniel Meilak <daniel.mlak@gmail.com>
2021-12-13 20:24:39 +0100

    wrote another (slower but more interesting) solution to part 2

--- week_2/day_13/README.md
+++ week_2/day_13/README.md
@@ -1,1 +1,7 @@
 ## Day 13
+
+I spent some time thinking about this puzzle after solving it the straight forward way. I assumed that there must be a way to know which points in the grid collapse onto all others without doing any "folding". So I took out some paper and worked out the patters (with help from Wolfram Alpha). In the end, I found this relation between points on a folding grid:
+
+In 1D, if you have a line of length `S`, the box `line[x]` is collapsed onto by all boxes: `line[ 0.5(2S*n + (S-2x-2)(-1)^n - (S+2)) ]` where `n` is the index for each repeating segment. `n=1` for the final collapsed line and `n=2,3,4...` for repeated lines.
+
+This method is implemented in [day_13_alt_part_2.cpp](day_13_alt_part_2.cpp), though it actaully runs slower than the straight forward solution. I assume this is because by folding you actually reduce the number of checked grid positions in half every time you fold the grid, making the total number of checked boxes not much higher, and also using the above function so many times has its own cost.
---
+++ week_2/day_13/day_13_alt_part_2.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include"../../Utils/utils.h"
+
+int main(){
+
+    // read dots
+    std::vector<std::string> delimiters = {","};
+    std::vector<std::vector<int>> dots  = input_to_int_2D(read_input_2D("dots", delimiters));
+
+    // read folds
+    delimiters = {"fold along ", "="};
+    std::vector<std::vector<std::string>> folds = read_input_2D("folds", delimiters);
+
+    // get number of repeated grids in each direction and initial & final grid size
+    int x_repeats = 1, y_repeats = 1;
+    int xmin = 0, ymin = 0, xmax = 0, ymax = 0;
+    for (const auto& line : folds){
+        if (line[0] == "x"){
+            x_repeats *= 2;
+            if (!xmax){ xmax = std::stoi(line[1])*2+1; }
+            xmin = std::stoi(line[1]);
+        }
+        else               {
+            y_repeats *= 2;
+            if (!ymax){ ymax = std::stoi(line[1])*2+1; }
+            ymin = std::stoi(line[1]);
+        }
+    }
+
+    // create and fill grid
+    std::vector<std::vector<bool>> grid(ymax, std::vector<bool>(xmax,false));
+    for (const auto& line : dots){ grid[line[1]][line[0]] = true; }
+
+    // loop through final grid
+    for (int x=0; x<xmin; x++){
+        for (int y=0; y<ymin; y++){
+
+            // loop through all unfolded grids
+            for (int nx=1; nx<=x_repeats; nx++){
+                int i = 0.5*( 2*(xmin+1)*nx + (xmin -2*x -1)*std::pow(-1,nx) - (xmin+2) );
+
+                for (int ny=1; ny<=y_repeats; ny++){
+                    int j = 0.5*( 2*(ymin+1)*ny + (ymin -2*y -1)*std::pow(-1,ny) - (ymin+2) );
+
+                    if (grid[j][i]){
+                        grid[y][x] = true;
+                        goto next_point;
+                    }
+                }
+            }
+next_point:;
+        }
+    }
+
+    // reduce grid to final state for printing
+    grid.erase(std::next(grid.begin(),ymin), grid.end());
+    for (auto& row : grid){ row.erase(std::next(row.begin(),xmin),row.end()); }
+
+    std::cout << "Answer (part 2): " << std::endl;
+    display(grid);
+
+    return 0;
+}
