f46920bbe52afd47b571869d0aa5b25434a5b350
Daniel Meilak <daniel.mlak@gmail.com>
2021-12-30 18:19:03 +0100

    swapped code versions for day 6

--- week_1/day_6/day_6_old.cpp
+++ week_1/day_6/day_6_old.cpp
@@ -21,17 +21,14 @@ int main(){
     // simulate school evolution for 80 days
     const int days = 256;
     long part_1, part_2;
-    auto it = stages.begin();
     for (int i=0; i<days; i++){
 
-        // instead of rotating the vector, keep track of fish on their "day 0"
-        // and add them to the day 2 behind (cyling from beg to end if required)
-        if (it <= stages.begin()+1 ){ *std::next(it,7) += *it; }
-        else { *std::next(it,-2) += *it; }
+        // move stages down by rotating the vector
+        std::rotate(stages.begin(), stages.begin()+1, stages.end());
 
-        // move it forward to next "day 0"
-        if (++it == stages.end()){ it = stages.begin(); }
-           
+        // add back the reset fish to stage 6
+        stages[6] += stages[8];        
+
         // at 80 days record number of fish for part_1
         if (i == 79){ part_1 = std::accumulate(stages.begin(),stages.end(),0l); }
     }
