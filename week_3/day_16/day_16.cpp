70d41120dcfda59a5e179f7f8b9134cb2bb8c25f
Daniel Meilak <daniel.mlak@gmail.com>
2022-01-24 22:21:25 +0100

    minor fixes and converted long to long long for MSVC

--- week_3/day_16/day_16.cpp
+++ week_3/day_16/day_16.cpp
@@ -9,17 +9,17 @@
 #include"utils.h"
 
 // map of type_ID to operator
-const std::unordered_map<int, std::function<long(std::vector<long>&)>> operators = {
-   {0, [](auto& vec){ return  std::accumulate (vec.begin(), vec.end(), 0l); }                   }, // plus
-   {1, [](auto& vec){ return  std::accumulate (vec.begin(), vec.end(), 1l,std::multiplies()); } }, // multiply
-   {2, [](auto& vec){ return *std::min_element(vec.begin(), vec.end()); }                       }, // minimum
-   {3, [](auto& vec){ return *std::max_element(vec.begin(), vec.end()); }                       }, // maximum
-   {5, [](auto& vec){ return  vec.front() >  vec.back(); }                                      }, // greater than
-   {6, [](auto& vec){ return  vec.front() <  vec.back(); }                                      }, // less than
-   {7, [](auto& vec){ return  vec.front() == vec.back(); }                                      }  // equal
+const std::unordered_map<int, std::function<long long(std::vector<long long>&)>> operators = {
+   {0, [](auto& vec){ return  std::accumulate (vec.begin(), vec.end(), 0ll); }                   }, // plus
+   {1, [](auto& vec){ return  std::accumulate (vec.begin(), vec.end(), 1ll,std::multiplies()); } }, // multiply
+   {2, [](auto& vec){ return *std::min_element(vec.begin(), vec.end()); }                        }, // minimum
+   {3, [](auto& vec){ return *std::max_element(vec.begin(), vec.end()); }                        }, // maximum
+   {5, [](auto& vec){ return  vec.front() >  vec.back(); }                                       }, // greater than
+   {6, [](auto& vec){ return  vec.front() <  vec.back(); }                                       }, // less than
+   {7, [](auto& vec){ return  vec.front() == vec.back(); }                                       }  // equal
 };
 
-long read_packet(const std::string binary, int& version_sum, size_t& read_pos);
+long long read_packet(const std::string binary, int& version_sum, size_t& read_pos);
 
 int main(){
 
@@ -36,7 +36,7 @@ int main(){
 
    // calculate version sum and expression
    int part_1 = 0;
-   long part_2 = read_packet(binary,part_1, start_pos);
+   long long part_2 = read_packet(binary,part_1, start_pos);
 
    std::cout << "Answer (part 1): " << part_1 << std::endl;
    std::cout << "Answer (part 2): " << part_2 << std::endl;
@@ -44,9 +44,9 @@ int main(){
    return 0;
 }
 
-long read_packet(const std::string binary, int& version_sum, size_t& read_pos){
+long long read_packet(const std::string binary, int& version_sum, size_t& read_pos){
 
-   long output;
+   long long output;
 
    // first three bits are packet version
    version_sum += std::stol(binary.substr(read_pos,3), nullptr, 2);
@@ -69,7 +69,7 @@ long read_packet(const std::string binary, int& version_sum, size_t& read_pos){
          }
 
          // calculate op(result)
-         output = stoul(s,nullptr,2);
+         output = stoull(s,nullptr,2);
          break;
       }
       // operator
@@ -78,13 +78,13 @@ long read_packet(const std::string binary, int& version_sum, size_t& read_pos){
          auto op = operators.at(type_ID);
 
          // keep memory of calculated literals
-         std::vector<long> memory;
+         std::vector<long long> memory;
 
          // read by length of packets
          if (binary[read_pos] == '0'){
             
             // total length of contained subpackets
-            size_t read_length = read_pos + std::stoul(binary.substr(read_pos+1,15), nullptr, 2) + 16;
+            size_t read_length = read_pos + std::stoull(binary.substr(read_pos+1,15), nullptr, 2) + 16;
             read_pos += 16;
 
             // read packets until length is exceeded
--- week_3/day_21/day_21.cpp
+++ week_3/day_21/day_21.cpp
@@ -5,6 +5,8 @@
 #include<unordered_map>
 #include"utils.h"
 
+using ullong = unsigned long long;
+
 struct game_t{
     int score1 = 0;
     int score2 = 0;
@@ -34,7 +36,7 @@ struct hash{
 const std::unordered_map<int,int> outcomes = {{3,1},{4,3},{5,6},{6,7},{7,6},{8,3},{9,1}};
 
 bool play(game_t& game, int& die);
-void play2(std::unordered_map<game_t,ulong,hash>& all_games, ulong& wins1, ulong& wins2);
+void play2(std::unordered_map<game_t,ullong,hash>& all_games, ullong& wins1, ullong& wins2);
 
 int main(){
 
@@ -51,11 +53,11 @@ int main(){
 
     // part 2
     // map of game and number of occurences (in all universes)
-    std::unordered_map<game_t,ulong,hash> all_games;
+    std::unordered_map<game_t,ullong,hash> all_games;
     all_games[game_t(input[0][1],input[1][1])] = 1;
 
     // number of wins
-    ulong wins1 = 0, wins2 = 0;
+    ullong wins1 = 0, wins2 = 0;
 
     while(!all_games.empty()){
 
@@ -63,7 +65,7 @@ int main(){
         play2(all_games,wins1,wins2);
     }
 
-    ulong part_2 = std::max(wins1,wins2);
+    ullong part_2 = std::max(wins1,wins2);
 
     std::cout << "Answer (part 1): " << part_1 << std::endl;
     std::cout << "Answer (part 2): " << part_2 << std::endl;
@@ -88,10 +90,10 @@ bool play(game_t& game, int& die){
     return game.score1 < 1000 && game.score2 < 1000;
 }
 
-void play2(std::unordered_map<game_t,ulong,hash>& all_games, ulong& wins1, ulong& wins2){
+void play2(std::unordered_map<game_t,ullong,hash>& all_games, ullong& wins1, ullong& wins2){
 
     // next set of games
-    std::unordered_map<game_t,ulong,hash> next_games;
+    std::unordered_map<game_t,ullong,hash> next_games;
 
     for (const auto& [game,identicals] : all_games){
         for (int i=3; i<=9; i++){
@@ -106,7 +108,7 @@ void play2(std::unordered_map<game_t,ulong,hash>& all_games, ulong& wins1, ulong
                 copy.score1 += copy.pos1;
             }
             copy.who = !copy.who;
-            ulong new_identicals = identicals*outcomes.at(i);
+            ullong new_identicals = identicals*outcomes.at(i);
 
             if      (copy.score1 >= 21){ wins1 += new_identicals; }
             else if (copy.score2 >= 21){ wins2 += new_identicals; }
